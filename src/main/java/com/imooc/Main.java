e33ff82a47900c9290bccbfdb59b172eb191e7a3
jyf1999 <1042767494@qq.com>
2023-03-22 20:13:06 +0800

    test commit

--- src/main/java/com/imooc/Main.java
+++ src/main/java/com/imooc/Main.java
@@ -1,4 +1,6 @@
-package com.imooc;public class Main {
+package com.imooc;
+
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
--- src/main/java/com/imooc/netty/HelloServer.java
+++ src/main/java/com/imooc/netty/HelloServer.java
@@ -1,2 +1,29 @@
-package com.imooc.netty;public class HelloServer {
+package com.imooc.netty;
+
+//实现客户端发送请求，服务的返沪hello netty
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+public class HelloServer {
+    public static void main(String[] args) throws Exception{
+        NioEventLoopGroup bossGroup = new NioEventLoopGroup();
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        ServerBootstrap serverBootstrap = new ServerBootstrap();
+        serverBootstrap
+                .group(bossGroup, workerGroup)
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                    protected void initChannel(NioSocketChannel ch) {
+                    }
+                });
+
+        ChannelFuture channelFuture =serverBootstrap.bind(8000).sync();
+        channelFuture.channel().closeFuture().sync();
+    }
 }
